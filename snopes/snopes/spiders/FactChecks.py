686015618f7bd7e9d4659de15d0a76596c0cd686
mohsenMahmoodzadeh <1mohsen.smm@gmail.com>
2021-11-07 02:24:12 +0330

    url_set & headers added

--- snopes/snopes/spiders/FactChecks.py
+++ snopes/snopes/spiders/FactChecks.py
@@ -5,6 +5,10 @@ class FactchecksSpider(scrapy.Spider):
     name = 'FactChecks'
     allowed_domains = ['http://www.snopes.com']
     start_urls = ['http://http://www.snopes.com/fact-check/']
+    url_set = set({"https://www.snopes.com/fact-check/"})
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0"
+    }
 
     def parse(self, response):
         pass
